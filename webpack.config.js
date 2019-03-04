c145447a8a14e91ee21b1b996e04eca8f1513e3d
yaya <yanis.saheb@gmail.com>
2019-03-04 16:35:12 -0500

    Setup dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = (env) => {
     return {
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, "public"),
+        path: path.join(__dirname, "public", "dist"),
         filename: "bundle.js"
     },
     // loader - run file through babel
@@ -53,7 +53,8 @@ module.exports = (env) => {
     devtool: isProduction? "source-map" :'cheap-module-eval-sourcemap',
     devServer : {
         contentBase: path.join(__dirname, "public"),
-        historyApiFallback: true
+        historyApiFallback: true,
+        publicPath: "/dist/"
     }
 }
 };
